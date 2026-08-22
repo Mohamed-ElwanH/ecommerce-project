b932e3973e1721fedf250721256b71f8a9e72037
Mohamed Hamza <mohamedelwan260@gmail.com>
2026-08-22 21:09:41 +0300

    product model

---
+++ server/models/product.model.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    stock: {
+      type: String,
+      required: true,
+      enum: ["Available", "Sold out"],
+    },
+    images: [{ type: String, required: true }],
+    category: {
+      type: String,
+      required: true,
+      enum: ["Men", "Women"],
+    },
+    subCategory: [{ sub: { type: String, required: true } }],
+
+    //slug is how the id becomes the product name in the url
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    isActive: {
+      //for visiblity
+      type: Boolean,
+      default: true,
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+    isTopSale: {
+      type: Boolean,
+      default: false,
+    },
+    isNewArrival: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+module.exports = mongoose.model("Product", productSchema);
